ad1127eddf8cf1459281b3bb95e870eb308c0222
kuldeepparmar246 <kuldeepparmar246@gmail.com>
2025-02-24 19:39:07 +0530

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express();
-app.use(express.json())
+
 app.use(express.static('dist'));
+
+const cors = require('cors')
+
+app.use(cors())
+
+
 
 const requestLogger = (request,response,next) => {
     console.log('Method : ',request.method)
@@ -11,6 +17,7 @@ const requestLogger = (request,response,next) => {
     next()
 }
 
+app.use(express.json())
 app.use(requestLogger)
 
 let notes = [
